c4654e82722836ebcff1d96066c7c0352d72a098
poesdro <98529959+poesdro@users.noreply.github.com>
2024-04-18 00:04:28 +0000

    comito1

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
-
-#Determina o nome do host
+from flask import Flask, render_template, g
+import sqlite3
 app = Flask("Ola")
-
-#Define uma rota(uma pagina)
+DATABASE  = "banco.bd"
+SECRET_KEY = "1234"
+app.config.from_object(__name__)
+def conectar():
+    return sqlite3.connect(DATABASE)
+@app.before_request
+def before_request():
+    g.bd = conectar()
+@app.teardown_request    
+def teardown_request(f):
+    g.bd.close()    
 @app.route('/')
-def ola():
-    #Mostro onde esta o arquivo .html para ser mandada(isso atravez do ginja)
-    return render_template('ola.html')
+def exibir_posts():
+    
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
+    resultado  = g.bd.execute(sql)
+    
+    posts = [
+        {"titulo": "Memórias Póstumas de Brás Cubas", "texto":"Fui descalçar as botas, que estavam apertadas.", "data_criacao": "1881"},
+        {"titulo": "Auto da Compadecida", "texto": "Matar Padre dá um azar danado", "data_criacao":"1955"}
+            ]
+    return render_template("ola.html", post=posts)
